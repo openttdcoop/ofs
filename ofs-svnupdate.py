5640f4f9c3e66e195719a0099531fe1be8151b5f
T. Werkhoven <t.werkhoven@turbulent-t.com>
2013-10-12 21:53:32 +0100

    Add: ofs-svntobin for copying the bundle to the actual server dir

--- ofs-svnupdate.py
+++ ofs-svnupdate.py
@@ -22,14 +22,13 @@
 # valid values: 'nightlies/trunk', 'stable' and 'testing' (same as stable, but also includes rc's)
 branch = 'nightlies/trunk'
 # directory where you keep the svn checkout. The script will run svn with this directory as working dir
-sourcedir = './'
+sourcedir = '../'
 
 
 
 # -------------------- DO NOT EDIT ANYTHING BELOW THIS LINE --------------------
 
 import re
-from shutil import copy2 as fileCopy
 from subprocess import Popen, PIPE, CalledProcessError
 from sys import exit
 import os, os.path
@@ -53,22 +52,16 @@ def main():
     if not os.path.isdir(sourcedir):
         exit(ReturnValues.get('FAILNOSOURCEDIR'))
 
-    # we'll want to work from sourcedir from here on out
+    # we'll want to work from sourcedir for svn update and make bundle
     os.chdir(sourcedir)
 
     if not execute(svnCommand, shell = True):
         exit(ReturnValues.get('FAILUPDATEERROR'))
 
-    # copy openttd.cfg from the bundle directory to safeguard it as make bundle erases the bundle directory
-    fileCopy(os.path.join(sourcedir, 'bundle/openttd.cfg'), os.path.join(sourcedir, 'openttd.cfg'))
-
     if not execute('make bundle', shell = True):
         exit(ReturnValues.get('FAILUPDATEERROR'))
 
-    # copy openttd.cfg back to the bundle directory, leaving a copy in the sourcedir on purpose as backup
-    fileCopy(os.path.join(sourcedir, 'openttd.cfg'), os.path.join(sourcedir, 'bundle/openttd.cfg'))
-    
-    print 'Successfully updated OpenTTD to %s' % newRevision
+    print 'Successfully updated OpenTTD SVN repository to %s' % newRevision
     exit(ReturnValues.get('SUCCESS'))
 
 def execute(command, shell = False):
@@ -104,10 +97,10 @@ def getLatestVersion(branch):
 
 def assignReturnValues():
     values = {
-        'SUCCESS'           : 0x00, # OpenTTD started successfully, pid written to openttd.pid
-        'FAILNOSOURCEDIR'   : 0x02, # Source directory does not exist
-        'FAILINVALIDBRANCH' : 0x03, # Config file contains an invalid branch
-        'FAILUPDATEERROR'   : 0x04, # SVN or make failed to run successfully
+        'SUCCESS'           : 0x00, # OpenTTD updated successfully
+        'FAILNOSOURCEDIR'   : 0x01, # Source directory does not exist
+        'FAILINVALIDBRANCH' : 0x02, # Config file contains an invalid branch
+        'FAILUPDATEERROR'   : 0x03, # SVN or make failed to run successfully
     }
     return values
 
--- ofs/README.txt
+++ ofs/README.txt
@@ -1,5 +1,5 @@
 Openttd File Scripts is just that, a collection of scripts to make
-updating/starting and downloading savegames a bit easier. 
+updating/starting and downloading savegames a bit easier.
 This collection is released under the GPL, a copy of which can be found in
 COPYING.txt
 
@@ -19,3 +19,16 @@ Use cases:
  * The Soap plugin for supybot also makes use of these scripts for the functions
    they provide, wether the server is local or remote. As such they need to be
    installed on every server you manage with Soap
+
+What does each file do?
+* ofs-getsave.py: Takes an URL for an argument, then saves the file into
+  OpenTTD's savegame directory
+* ofs-start.py: What it says on the tin, starts OpenTTD as a dedicated server,
+  and will load the most recent autosave found in the autosave directory.
+* ofs-svnupdate.py and ofs-svntobin.py: ofs-svnupdate.py will run svn and update
+  the repository to the most recent version of the branch, then will run make
+  bundle to compile the bundle. ofs-svntobin.py is then called to copy the
+  bundle-files to the directory where OpenTTD actually runs from. These actions
+  were split up to allow the calling program to shut down the server before
+  copying the files thus allowing the server to stay up until it has to be
+  shut down.
